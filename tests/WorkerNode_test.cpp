2a4c20dd4200cade7c030132a83c51a7d4115f5c
Kikito <kylian.jozefiak@etu.unistra.fr>
2024-08-07 17:22:50 +0200

    added tests for worker

--- tests/WorkerNode_test.cpp
+++ tests/WorkerNode_test.cpp
@@ -62,6 +62,66 @@ TEST_F(WorkerNodeTest, ProcessSingleTaskTest)
     mock_db_handler_ = std::make_unique<MockDatabaseHandler>("config.ini");
 }
 
+TEST_F(WorkerNodeTest, ProcessMultipleTasksTest)
+{
+    user_analysis::Task task1, task2;
+    task1.set_id("test_task_1");
+    task1.set_start_user_id(1);
+    task1.set_end_user_id(50);
+    task2.set_id("test_task_2");
+    task2.set_start_user_id(51);
+    task2.set_end_user_id(100);
+
+    EXPECT_CALL(*mock_task_interface_, GetTask(testing::_, testing::_, testing::_))
+        .WillOnce(testing::DoAll(
+            testing::SetArgPointee<2>(task1),
+            testing::Return(grpc::Status::OK)))
+        .WillOnce(testing::DoAll(
+            testing::SetArgPointee<2>(task2),
+            testing::Return(grpc::Status::OK)))
+        .WillOnce(testing::Return(grpc::Status(grpc::StatusCode::NOT_FOUND, "")));
+
+    EXPECT_CALL(*mock_db_handler_, executeQuery(testing::_))
+        .WillOnce(testing::Return(pqxx::result{}))
+        .WillOnce(testing::Return(pqxx::result{}));
+
+    EXPECT_CALL(*mock_task_interface_, SubmitResult(testing::_, testing::_, testing::_))
+        .WillOnce(testing::Return(grpc::Status::OK))
+        .WillOnce(testing::Return(grpc::Status::OK));
+
+    WorkerNode worker(std::move(mock_task_interface_), std::move(mock_db_handler_));
+    worker.Run();
+}
+
+TEST_F(WorkerNodeTest, GetTaskErrorTest)
+{
+    EXPECT_CALL(*mock_task_interface_, GetTask(testing::_, testing::_, testing::_))
+        .WillOnce(testing::Return(grpc::Status(grpc::StatusCode::INTERNAL, "Internal error")));
+
+    WorkerNode worker(std::move(mock_task_interface_), std::move(mock_db_handler_));
+    worker.Run();
+}
+
+TEST_F(WorkerNodeTest, ExecuteQueryErrorTest)
+{
+    user_analysis::Task task;
+    task.set_id("test_task_1");
+    task.set_start_user_id(1);
+    task.set_end_user_id(100);
+
+    EXPECT_CALL(*mock_task_interface_, GetTask(testing::_, testing::_, testing::_))
+        .WillOnce(testing::DoAll(
+            testing::SetArgPointee<2>(task),
+            testing::Return(grpc::Status::OK)))
+        .WillOnce(testing::Return(grpc::Status(grpc::StatusCode::NOT_FOUND, "")));
+
+    EXPECT_CALL(*mock_db_handler_, executeQuery(testing::_))
+        .WillOnce(testing::Throw(std::runtime_error("Database error")));
+
+    WorkerNode worker(std::move(mock_task_interface_), std::move(mock_db_handler_));
+    worker.Run();
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
